7d11d60de68fcba2c0b6d1b81c60277c7df516ab
Vadim2712 <vad_gul2005@mail.ru>
2025-09-24 13:39:13 +0300

    Добавлен базовый роутинг: LoginPage и DashboardPage

---
+++ defect-manager/src/pages/DashBoardPage.js
@@ -0,0 +1,7 @@
+export default function DashboardPage() {
+    return (
+        <div className="p-6">
+            <h1 className="text-2xl font-bold">Панель управления</h1>
+        </div>
+    )
+}
