c4930be94a920dff9315408c65045687ee3ef1b8
Vadim2712 <vad_gul2005@mail.ru>
2025-09-24 13:41:53 +0300

    Добавлен сервис работы с Local Storage

---
+++ defect-manager/src/services/storageService.js
@@ -0,0 +1,17 @@
+const storageService = {
+    get(key) {
+        const data = localStorage.getItem(key)
+        return data ? JSON.parse(data) : null
+    },
+    set(key, value) {
+        localStorage.setItem(key, JSON.stringify(value))
+    },
+    remove(key) {
+        localStorage.removeItem(key)
+    },
+    clear() {
+        localStorage.clear()
+    }
+}
+
+export default storageService
